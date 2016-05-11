4b303705175ba854707799416a70bb3b31ba4ebf
Zach Phillips <zachphillips@wcu.edu>
2016-05-11 13:55:25 -0400

    Added stylesheet and fade on name

--- Main.js
+++ Main.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
-    if (jQuery) {
+    /*if (jQuery) {
      // jQuery is loaded
     alert("Yeah!");
     } else {
     // jQuery is not loaded
     alert("Doesn't Work");
-        }
-
+	}*/
+$('#inputbox').focus();
+//$('#inputbox').css("visibility", "hidden");
 
 var typingTimer;                //timer identifier
-var doneTypingInterval = 5000;  //time in ms, 5 second for example
+var doneTypingInterval = 2000;  //time in ms, 5 second for example
 
 //on keyup, start the countdown
 $('#inputbox').keyup(function(){
@@ -24,7 +25,25 @@ function doneTyping () {
     //do something
 		inputText = document.getElementById("inputbox").value;
 		document.getElementById("inputbox").value = "";
-		document.getElementById("display").innerHTML = inputText;
+		//$("#gradtext").text(inputText);
+		$('#gradtext').addClass('animated fadeOut').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+			function(){
+
+				$(this).text(inputText);
+				$(this).css("visibility", "hidden");
+				$(this).removeClass('animated fadeOut');
+				$(this).css("visibility", "hidden");
+				$(this).addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
+					function(){
+						$(this).removeClass('animated fadeIn');
+					});
+				$(this).css("visibility", "visible");
+				//$(this).removeClass('animated fadeIn');
+			});
+
+
+
+		//$('#gradtext').
 }
 
 /*
