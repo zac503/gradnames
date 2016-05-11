69ad233d95794857995a33f58aa10c47939a811d
Zach Phillips <zachphillips@wcu.edu>
2016-05-11 15:10:04 -0400

    Tweaked and cleaned up code

--- Main.js
+++ Main.js
@@ -1,16 +1,9 @@
 $(document).ready(function(){
-    /*if (jQuery) {
-     // jQuery is loaded
-    alert("Yeah!");
-    } else {
-    // jQuery is not loaded
-    alert("Doesn't Work");
-	}*/
+
 $('#inputbox').focus();
-//$('#inputbox').css("visibility", "hidden");
 
 var typingTimer;                //timer identifier
-var doneTypingInterval = 2000;  //time in ms, 5 second for example
+var doneTypingInterval = 500;  //time in ms, 5 second for example
 
 //on keyup, start the countdown
 $('#inputbox').keyup(function(){
@@ -32,38 +25,12 @@ function doneTyping () {
 				$(this).text(inputText);
 				$(this).css("visibility", "hidden");
 				$(this).removeClass('animated fadeOut');
-				//$(this).css("visibility", "hidden");
 				$(this).addClass('animated fadeIn').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',
 					function(){
 						$(this).removeClass('animated fadeIn');
 					});
 				$(this).css("visibility", "visible");
-				//$(this).removeClass('animated fadeIn');
 			});
+ }
 
-
-
-		//$('#gradtext').
-}
-
-/*
-//var inputBox = document.getElementById("inputbox");
-var running = false;
-
-function myFunction() {
-
-if(running == false){
-	runing = true;
-	var inputText;
-	setTimeout(function(){
-		inputText = document.getElementById("inputbox").value;
-		document.getElementById("inputbox").value = "";
-		document.getElementById("display").innerHTML = inputText;
-		},1000);
-	running = false;
-}
-
-
-}*/
-
-    });
+});
